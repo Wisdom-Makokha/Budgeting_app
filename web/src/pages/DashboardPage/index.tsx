54f6cee574ef57dff3a91d73870b827088b30189
DavidManga254 <mangadavid2@gmail.com>
2025-01-20 19:20:54 +0300

    add summarry chart

--- web/src/pages/DashboardPage/index.tsx
+++ web/src/pages/DashboardPage/index.tsx
@@ -1,12 +1,13 @@
 import FinanceCard from "@components/FinanceCard";
 import "./index.css";
 import TransactionHistory from "@components/TransactionHistory";
+import SpendingChart from "@components/SpendingChart";
 
 interface DashboardPageInterface {}
 
 const DashboardPage: React.FC<DashboardPageInterface> = ({}) => {
     return (
-        <div className="dashboard-page">
+        <div className="dashboard-page ">
             <div className="finance-cards">
                 <FinanceCard title="Total Money" amount={20000} />
                 <FinanceCard
@@ -16,6 +17,7 @@ const DashboardPage: React.FC<DashboardPageInterface> = ({}) => {
                 />
                 <FinanceCard title="Expenses this month" amount={10000} />
             </div>
+            <SpendingChart />
             <TransactionHistory />
         </div>
     );
